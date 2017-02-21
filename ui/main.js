85ab7d9ac82f24d165fc5cf3ecc6082b0ee858a7
Abhishek Kumar Gupta <abhi962704@users.noreply.github.com>
2017-02-22 00:26:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
