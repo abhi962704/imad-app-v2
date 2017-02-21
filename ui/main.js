5dffed84403618c6310e3d65352f93f3fda75562
Abhishek Kumar Gupta <abhi962704@users.noreply.github.com>
2017-02-22 00:29:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,4 +5,11 @@ function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
   myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
+}if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}myButton.onclick = function() {
+  setUserName();
+};
