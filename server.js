9b4ea164caa403382c5f0538eecda654fa5d5c68
Abhishek Kumar Gupta <abhi962704@users.noreply.github.com>
2017-03-16 10:54:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-app.get('/ui/pencil-HD.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'pencil-HD.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
